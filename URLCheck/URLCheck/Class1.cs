0a80c14e0c2017de23a5eabc4fc361a1bcef8aac
rnara <rajakumarsnara@outlook.com>
2018-08-06 14:40:52 +0000

    v1

---
+++ URLCheck/URLCheck/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+namespace URLCheck
+{
+    public class Class1
+    {
+        //string url;
+        //void getURL(string _url)
+        //{
+        //    this.url = _url;
+        //}
+        public string processURL(string url)
+        {
+            try
+            {
+                //Creating the HttpWebRequest
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                //Setting the Request method HEAD, you can also use GET too.
+                request.Method = "HEAD";
+                //Getting the Web Response.
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                //Returns TRUE if the Status code == 200
+                response.Close();
+                return (response.StatusCode).ToString();
+            }
+            catch(Exception ex)
+            {
+                //Any exception will returns false.
+                return ex.Message.ToString();
+            }           
+        }
+
+       
+
+    }
+}
